f7f0ac6cab4d4c76b7417d0ffb7effea6a77788e
Fernanda Ferreira <fernanda.ferreira@nuvem.net>
2020-08-25 09:42:00 -0400

    Adiciona operadores

---
+++ operadores.js
@@ -0,0 +1,34 @@
+// Operadores Aritméticos:
+// 2+2 = 4
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(8/4);
+
+// Operadores de Comparação
+// igual
+// console.log(2 == 2);
+// console.log(2 == 3);
+// console.log(2 == "2"); // retorna true, pois compara apenas o valor e não o tipo
+// diferente
+// console.log(2 != 2);
+// console.log(2 != 3);
+// igual estrito -> valor e tipo igual
+// console.log(2 === "2"); // retorna false, pois compara o valor e o tipo
+
+// diferente estrito -> valor e tipo diferente
+// console.log(3 !== "3");
+
+// Maior que >
+console.log(2 > 1);
+
+// Menor que <
+console.log(1 < 2);
+
+// Maior e igual >=
+console.log(2 >= 2);
+console.log(2 >= 1);
+
+// Menor e igual <=
+console.log(2 <= 2);
+console.log(1 <= 2);
