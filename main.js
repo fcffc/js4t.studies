6784a58a14621a5d2b3a249714aa4ba7fa3ca715
Fernanda Ferreira <fernanda.ferreira@nuvem.net>
2020-08-25 08:48:10 -0400

    Adiciona variaveis

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 var welcome = "Hello JavaScript automated full stack from QA"
 document.getElementById("welcome").append(welcome);
 console.log(welcome);
+
+var numeber = 10;
+console.log(numeber);
+
+// typeof é responsável por trazer o tipo da variável
+console.log(typeof numeber)
+
+var nome = "Fernanda"
+console.log(typeof nome)
+
+var n1 =10, n2=20;
+console.log(n1 + n2);
+
+var n1 =10, n2="20";
+console.log(n1 + n2);
