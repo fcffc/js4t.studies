e96dd2451d10d9d00d4677bd30003efffa2c80a2
Fernanda Ferreira <fernanda.ferreira@nuvem.net>
2020-08-25 23:08:56 -0400

    Adiciona function

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-var welcome = "Hello JavaScript automated full stack from QA"
-document.getElementById("welcome").append(welcome);
-console.log(welcome);
+
+function welcome() {
+    var welcome = "Hello JavaScript automated full stack from QA"
+    document.getElementById("welcome").append(welcome);
+    console.log(welcome);
+}
+
+welcome();
+
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+console.log(soma(15, 25));
